8b087f37dbd784633fe6446b794e651f0e4ffb96
Elliott <ellduc4@gmail.com>
2023-07-22 21:42:21 -0500

    First version of "bot"

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,12 +1,24 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Linq;
+using ChessChallenge.API;
 
 namespace Chess_Challenge.My_Bot;
 
 public class MyBot : IChessBot
 {
+	private static readonly Random random = new();
+	
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+	    Move[] moves = board.GetLegalMoves();
+	    Move move = moves.FirstOrDefault(m => m.MovePieceType == PieceType.King);
+
+	    if (move == default)
+	    {
+		    int index = random.Next(0, moves.Length);
+		    move = moves[index];
+	    }
+	    
+	    return move;
     }
 }
