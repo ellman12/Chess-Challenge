f3d09dd2fb8b031f6e57df9bd55b21deef1367b5
Elliott <ellduc4@gmail.com>
2023-07-23 11:54:13 -0500

    Slightly less terrible bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,15 +7,22 @@ namespace Chess_Challenge.My_Bot;
 public class MyBot : IChessBot
 {
 	private static readonly Random random = new();
-	
+
     public Move Think(Board board, Timer timer)
     {
 	    Move[] moves = board.GetLegalMoves();
-	    Move move = moves.FirstOrDefault(m => m.MovePieceType == PieceType.King);
-
-	    if (move == default)
+	    Move move;
+	    int index;
+	    
+	    Move[] kingMoves = moves.Where(m => m.MovePieceType == PieceType.King).ToArray();
+	    if (kingMoves.Length > 0)
 	    {
-		    int index = random.Next(0, moves.Length);
+		    index = random.Next(0, kingMoves.Length);
+		    move = kingMoves[index];
+	    }
+	    else
+	    {
+		    index = random.Next(0, moves.Length);
 		    move = moves[index];
 	    }
 	    
